ea507e06ec0e97cfe3b446489f7826b93e129dcc
CharithAnandika <charithanandika4@gmail.com>
2025-06-27 16:27:57 +0530

    day04_2

--- Day 3/copyfile.py
+++ Day 3/copyfile.py
@@ -1,16 +1,20 @@
 import os
+import shutil
 
 source_file = "source_doc.txt"
 with open(source_file, "w") as f:
     f.write("this is original content.")
-print(f"created '{source_file}' for copying.")
+print(f"Created '{source_file}' for copying.")
 
-#copy the file
+# Copy the file
 destination_file = "copied_doc.txt"
-try : 
-    shutil.copy(source_file,destination_file)
+try:
+    shutil.copy(source_file, destination_file)
     print(f"'{source_file}' copied to '{destination_file}' successfully.")
-    print(f"contect of '{destination_file}' : '{open(destination_file)}' ")
 
-except FileExistsError:
-    print(f"error : source file '{source_file}' not found")
+    with open(destination_file, "r") as f:
+        content = f.read()
+    print(f"Content of '{destination_file}': '{content}'")
+
+except FileNotFoundError:
+    print(f"Error: Source file '{source_file}' not found.")
